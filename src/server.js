450d906f9a0accde8f480a1d7d3d74811ed92b33
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-07-20 11:10:01 -0300

    Método HTTP

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/message",(request,response)=>{
+    response.send("Hello, world!")
+})
+
+const port = 3333
+app.listen(port, () => console.log(`Server is running on port:${port}`))
