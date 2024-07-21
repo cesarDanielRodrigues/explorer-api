301086d2b67724b1e0444bb0312a80b88e387956
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-07-21 16:53:30 -0300

    Utilizando middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,20 @@
-const {Router} = require("express")
+const { Router } = require("express")
 const UsersController = require("../controllers/UsersController")
 
-
 const usersRoutes = Router()
+
+// function myMiddleware(request, response, next) {
+//   if (!request.body.isAdmin) {
+//     return response.json({ message: "erro" })
+//   }
+
+//   next()
+// }
+
 const usersController = new UsersController()
 
+// usersRoutes.use(myMiddleware)
 usersRoutes.post("/", usersController.create)
+// usersRoutes.post("/", myMiddleware, usersController.create)
 
 module.exports = usersRoutes
