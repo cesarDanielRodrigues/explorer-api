1a0d5aeaacc23e6e2fc63459e5cade53c555dbd3
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-07-30 15:31:22 +0000

    Conectando com o bando de dados

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors")
+const database = require("./database/sqlite")
+
 const AppError = require("./utils/AppError")
 
 const express = require("express")
@@ -8,7 +10,11 @@ const routes = require("./routes")
 const app = express()
 app.use(express.json())
 
+
 app.use(routes)
+
+database()
+
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
