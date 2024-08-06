8275d0df4622d7e44c4f4490dd746b9bbf22c587
cesar <cesar.daniel.rodrigues@gmail.com>
2024-08-06 11:22:27 -0300

    migrations

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
 
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(routes)
 
-database()
+migrationRun()
 
 
 app.use((error, request, response, next) => {
