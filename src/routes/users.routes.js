dc0a71716b469774fc22b4715478f37bdb126af1
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-08-15 16:43:44 -0300

    Atualizando usuário

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -15,6 +15,7 @@ const usersController = new UsersController()
 
 // usersRoutes.use(myMiddleware)
 usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update)
 // usersRoutes.post("/", myMiddleware, usersController.create)
 
 module.exports = usersRoutes
