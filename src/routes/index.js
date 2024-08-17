ab643713b25b9b5a3fa5381ee25c12b96df3412d
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-08-17 18:23:29 -0300

    Cadastrando nota

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const {Router} = require("express")
 
 const usersRoutes = require("./users.routes")
+const notesRoutes = require("./notes.routes")
 
 const route = Router()
 
 route.use("/users", usersRoutes)
+route.use("/notes", notesRoutes)
 
 module.exports = route
