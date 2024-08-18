7a30ea592b00d8e25e39c4c6f276854883ba02e7
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-08-18 10:57:22 -0300

    Deletando notas

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -9,9 +9,9 @@ class NotesController {
       title,
       description,
       user_id,
-    });
+    })
 
-    const linksInsert = links.map(link => {
+    const linksInsert = links.map((link) => {
       return {
         note_id,
         url: link,
@@ -19,20 +19,38 @@ class NotesController {
     })
 
     await knex("links").insert(linksInsert)
-    
 
-    const tagsInsert = tags.map(name =>{
-        return{
-            note_id,
-            name,
-            user_id
-        }
+    const tagsInsert = tags.map((name) => {
+      return {
+        note_id,
+        name,
+        user_id,
+      }
     })
 
-    await knex("tags").insert(tagsInsert)   
+    await knex("tags").insert(tagsInsert)
 
     response.json()
+  }
+  async show(request, response) {
+    const { id } = request.params
 
+    const note = await knex("notes").where({ id }).first()
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name")
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at")
+
+    response.json({
+      ...note,
+      tags,
+      links,
+    })
+  }
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex("notes").where({ id }).delete()
+
+    response.json()
   }
 }
 
