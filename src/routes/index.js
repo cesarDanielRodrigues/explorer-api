6617a60585533fa8a70d2d4a41f7c52e75dd23b6
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-08-23 06:17:21 -0300

    Listando tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const {Router} = require("express")
 
 const usersRoutes = require("./users.routes")
 const notesRoutes = require("./notes.routes")
+const tagsRoutes = require("./tags.routes")
 
 const route = Router()
 
 route.use("/users", usersRoutes)
 route.use("/notes", notesRoutes)
+route.use("/tags", tagsRoutes)
 
 module.exports = route
